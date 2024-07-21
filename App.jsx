123fd3bbf7859d201ebc355103c5e524bc45c7ad
AfzalA-12 <Afxal2003@gmail.com>
2024-07-22 01:42:37 +0530

    Add files via upload
    Main Code

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Hero from "./component/Hero";
+import Nav from "./component/navigation";
+const App = () => {
+  return (
+    <div>
+      <Nav />
+      <Hero />
+    </div>
+  );
+};
+
+export default App;
